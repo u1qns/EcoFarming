7a7ffad115df3b01826bf2342ac2b1ec3fd6a84e
KimJ4ehyun <kimjaehyun158@gmail.com>
2024-09-02 20:23:25 +0900

    ✨ feat: Challenge Entity & Repository 추가
    feat: Challenge Entity & Repository 추가

--- backend/src/main/java/com/a101/ecofarming/Challenge/entity/Challenge.java
+++ backend/src/main/java/com/a101/ecofarming/Challenge/entity/Challenge.java
@@ -1,4 +1,43 @@
 package com.a101.ecofarming.Challenge.entity;
 
+import com.a101.ecofarming.BalanceGame.entity.BalanceGame;
+import com.a101.ecofarming.ChallengeCategory.entity.ChallengeCategory;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "challenge_id")
+    private int id;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private int frequency;
+
+    @Column(nullable = false)
+    private int period;
+
+    private int totalBetMoneyOption1;
+
+    private int totalBetMoneyOption2;
+
+    @ManyToOne
+    @JoinColumn(name = "balance_id", nullable = false)
+    private BalanceGame balanceGame;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ChallengeCategory challengeCategory;
 }
--- backend/src/main/java/com/a101/ecofarming/ChallengeCategory/entity/ChallengeCategory.java
+++ backend/src/main/java/com/a101/ecofarming/ChallengeCategory/entity/ChallengeCategory.java
@@ -1,4 +1,10 @@
 package com.a101.ecofarming.ChallengeCategory.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class ChallengeCategory {
+    @Id
+    private int id;
 }
