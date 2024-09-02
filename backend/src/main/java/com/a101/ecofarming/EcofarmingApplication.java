b020596801a9a4407a440a912ac5265344bbdda8
김수빈 <u1qns@naver.com>
2024-09-02 15:28:30 +0900

    feat: 프로젝트 초기화

---
+++ backend/src/main/java/com/a101/ecofarming/EcofarmingApplication.java
@@ -0,0 +1,13 @@
+package com.a101.ecofarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcofarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcofarmingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/a101/ecofarming/EcofarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a101.ecofarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcofarmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
