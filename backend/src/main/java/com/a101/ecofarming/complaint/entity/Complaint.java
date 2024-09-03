5f90d75e3dc6c2d0b8fa7c45150985e1f7610b96
cmbear314 <cmbear314@naver.com>
2024-09-03 14:04:23 +0900

    :sparkles: Feat: Complaint 엔티티 생성

--- backend/src/main/java/com/a101/ecofarming/complaint/entity/Complaint.java
+++ backend/src/main/java/com/a101/ecofarming/complaint/entity/Complaint.java
@@ -1,4 +1,34 @@
 package com.a101.ecofarming.complaint.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "complaint")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Complaint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer complaintId;
+
+    @Column(nullable = false)
+    private Integer proofId;
+
+    @Column(nullable = false)
+    private Integer complainerId;
+
+    @Column
+    private String description;
+
+    @Column
+    private Boolean aiPass;
+
+    @Column
+    private Boolean adminPass;
+
 }
+
