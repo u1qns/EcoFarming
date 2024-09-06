2ae4ea7ea99b091cefbaa7473d1fc1ff12232e05
이찬민 <cmbear314@naver.com>
2024-09-06 14:49:03 +0900

    :sparkles: Feat: 밸런스게임, 챌린지유저 엔티티생성

--- backend/src/main/java/com/a101/ecofarming/balanceGame/entity/BalanceGame.java
+++ backend/src/main/java/com/a101/ecofarming/balanceGame/entity/BalanceGame.java
@@ -1,10 +1,29 @@
 package com.a101.ecofarming.balanceGame.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "balance_game")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BalanceGame {
+
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "balance_id")
+    private Integer balanceId;
+
+    @Column(name = "option1_description", nullable = false)
+    private String option1Description;
+
+    @Column(name = "option2_description", nullable = false)
+    private String option2Description;
+
 }
