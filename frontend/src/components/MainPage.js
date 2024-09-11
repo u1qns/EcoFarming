5401f917290276772540be5ebeebf699a7ec5fc5
ezeun <consistency1231@gmail.com>
2024-09-11 17:15:18 +0900

    ✨ Feat: Card 생성

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,12 +1,34 @@
 import "./MainPage.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Card from "./Card";
 
 function MainPage() {
+  const cardData = [
+    { title: "Card 1", content: "This is card 1 content" },
+    { title: "Card 2", content: "This is card 2 content" },
+    { title: "Card 3", content: "This is card 3 content" },
+    { title: "Card 4", content: "This is card 4 content" },
+    { title: "Card 5", content: "This is card 5 content" },
+    { title: "Card 6", content: "This is card 6 content" },
+    { title: "Card 7", content: "This is card 7 content" },
+    { title: "Card 8", content: "This is card 8 content" },
+    { title: "Card 6", content: "This is card 6 content" },
+    { title: "Card 7", content: "This is card 7 content" },
+    { title: "Card 8", content: "This is card 8 content" },
+  ];
+
   return (
     <div className="MainPage">
       <Navbar />
-      <div className="content">하츄핑피리핑핑핑</div>
+      <div className="content">
+        하츄핑피리핑핑핑
+        <div className="card-container">
+          {cardData.map((card, index) => (
+            <Card key={index} title={card.title} content={card.content} />
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   );
