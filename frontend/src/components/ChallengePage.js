37d4e9d0607549f55753e2680ebe4c450c273be4
ezeun <consistency1231@gmail.com>
2024-09-19 14:00:24 +0900

    ✨ Feat: ChallengeFooter 생성

--- frontend/src/components/ChallengePage.js
+++ frontend/src/components/ChallengePage.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { ChevronLeft, Share2, Heart } from "lucide-react";
 import { FaHeart, FaUser } from "react-icons/fa";
 import "./ChallengePage.css";
+import ChallengeFooter from "./ChallengeFooter";
 
 const ChallengePage = () => {
   const navigate = useNavigate();
@@ -22,46 +23,46 @@ const ChallengePage = () => {
   ];
 
   return (
-    <div className="challenge-page">
-      <div className="image-container">
-        <img
-          src={require("../assets/images/c1.jpg")}
-          alt="Challenge"
-          className="challenge-image"
-        />
-        <div className="header">
-          <div className="icon-background" onClick={handleBackClick}>
-            <ChevronLeft className="back-button" />
-          </div>
-          <div className="action-buttons">
-            <div className="icon-background">
-              <Share2 className="share-button" />
-            </div>
-            <div className="icon-background">
-              <Heart className="heart-button" />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="ChallengePage">
       <div className="content">
-        <div className="challenge-type">공식 챌린지</div>
-        <h1 className="challenge-title">안 쓰는 가전제품 콘센트 뽑기</h1>
-        <div className="challenge-stats">
-          <span className="rating">⭐ 4.8</span>
-          <span className="participants">
-            • <FaUser /> 현재 26명
-          </span>
+        <div className="image-container">
+          <img
+            src={require("../assets/images/c1.jpg")}
+            alt="Challenge"
+            className="challenge-image"
+          />
+          <div className="header">
+            <div className="icon-background" onClick={handleBackClick}>
+              <ChevronLeft className="back-button" />
+            </div>
+            <div className="action-buttons">
+              <div className="icon-background">
+                <Share2 className="share-button" />
+              </div>
+              <div className="icon-background">
+                <Heart className="heart-button" />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="challenge-duration">
-          <span className="duration-item">주 2일</span>
-          <span className="duration-item">2주 동안</span>
+        <div className="challenge-content">
+          <div className="challenge-type">공식 챌린지</div>
+          <h1 className="challenge-title">안 쓰는 가전제품 콘센트 뽑기</h1>
+          <div className="challenge-stats">
+            <span className="rating">⭐ 4.8</span>
+            <span className="participants">
+              • <FaUser /> 현재 26명
+            </span>
+          </div>
+          <div className="challenge-duration">
+            <span className="duration-item">주 2일</span>
+            <span className="duration-item">2주 동안</span>
+          </div>
         </div>
-      </div>
 
-      <hr />
-      <div className="challenge-content">
+        <hr />
         <div style={{ padding: 15 }}>
-          <h2>돈을 걸면 무조건 하게 됩니다.</h2>
+          <h2>돈을 걸면 무조건 하게 됩니다</h2>
           <p style={{ lineHeight: "1.5" }}>
             나를 움직이는 강력한 알람, 돈!
             <br />
@@ -141,7 +142,44 @@ const ChallengePage = () => {
           </div>
         </div>
         <hr />
+        <div style={{ padding: 15 }} className="proof">
+          <h2>이렇게 인증해주세요</h2>
+          <p style={{ lineHeight: "1.5" }}>
+            안 쓰는 가전제품의&nbsp;
+            <span style={{ fontWeight: "bold" }}>콘센트를 뽑은 후</span>의
+            사진을 찍어 인증해주세요!
+          </p>
+          <div className="proof-container">
+            <div className="image-wrapper">
+              <img
+                src={require("../assets/images/c1.jpg")}
+                alt="Left image"
+                className="proof-image"
+              />
+              <div className="green-bar">O</div>
+            </div>
+            <div className="image-wrapper">
+              <img
+                src={require("../assets/images/c1.jpg")}
+                alt="Right image"
+                className="proof-image"
+              />
+              <div className="red-bar">X</div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div style={{ padding: 15 }}>
+          <h2>챌린지 진행 시 꼭 알아주세요!</h2>
+          <p>
+            ✔&nbsp; 00시 00분 ~ 23시 59분 사이에 인증 하셔야
+            <br /> &nbsp; &nbsp; 합니다.
+          </p>
+          <p>✔&nbsp; 사진첩을 사용하실 수 없습니다.</p>
+          <p>✔&nbsp; 인증샷이 참가자에게만 공개됩니다.</p>
+        </div>
       </div>
+      <ChallengeFooter />
     </div>
   );
 };
