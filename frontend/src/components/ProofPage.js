2dc1e99cc4798f632bfb3ec9c9eb8d67d6435e55
ezeun <consistency1231@gmail.com>
2024-09-19 16:49:03 +0900

    ✨ Feat: 인증페이지 생성

---
+++ frontend/src/components/ProofPage.js
@@ -0,0 +1,13 @@
+import "./ProofPage.css";
+import Footer from "./Footer";
+
+function ProofPage() {
+  return (
+    <div className="ProofPage">
+      <div className="content">sdfgjskldfgjklsz</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default ProofPage;
