3fee7133b391592d2848fc686edd8b7c182bd23a
ezeun <consistency1231@gmail.com>
2024-09-24 15:33:10 +0900

    ✨ Feat: 참가자인증현황 만들기

--- frontend/src/components/ParticipantProofStatus.js
+++ frontend/src/components/ParticipantProofStatus.js
@@ -5,7 +5,32 @@ import "./ParticipantProofStatus.css";
 const ParticipantProofStatus = () => {
   return (
     <div className="ParticipantProofStatus">
-      <div>참가자 인증 현황을 보여주는 컴포넌트입니다.</div>
+      <div className="achievement-section">
+        <div className="achievement">
+          <p>총 참가자수</p>
+          <h1>34명</h1>
+        </div>
+        <div className="achievement">
+          <p>평균 예상 달성률</p>
+          <h1>80%</h1>
+        </div>
+      </div>
+      <div className="ProofImage">
+        <h2>참가자 인증샷</h2>
+        <div className="image-grid">
+          {Array(7)
+            .fill()
+            .map((_, index) => (
+              <div className="image-wrapper" key={index}>
+                <img
+                  src={require("../assets/images/c1.jpg")}
+                  alt={`인증 이미지 ${index + 1}`}
+                  className="image" // CSS 클래스 추가
+                />
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
