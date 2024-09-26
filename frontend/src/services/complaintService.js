9ef76f0c19306de55b7f4cf0547e342c3ec64946
김수빈 <u1qns@naver.com>
2024-09-26 10:32:25 +0900

    ♻️Refactor: fetch->axios 변경 - 자동 json 답변처리 및 http 오류 코드 반환 그리고 구형 브라우저에서 fetch 미 지원을 고려하여 변경 - `npm install axios` 설치 필요

--- frontend/src/services/complaintService.js
+++ frontend/src/services/complaintService.js
@@ -1,17 +1,16 @@
+import axios from 'axios';
+
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const submitComplaint = async (reportData) => {
-  const response = await fetch(`${API_URL}/complaints/proof`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(reportData),
-  });
-
-  if (!response.ok) {
+  try {
+    const response = await axios.post(`${API_URL}/complaints/proof`, reportData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data; // axios는 자동으로 JSON 변환을 해줍니다.
+  } catch (error) {
     throw new Error('Network response was not ok');
   }
-
-  return response.json();
 };
