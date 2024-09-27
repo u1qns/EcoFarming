3b7f782c4057f935495ffce312a7797c11568ea0
김수빈 <u1qns@naver.com>
2024-09-27 17:53:30 +0900

    📦Chore: 사용하지 않는 import문 제거 - FCM + userId (예) FCM12 와 Token값을 쌍으로 캐싱하기 위해 Redis 사용

--- backend/src/main/java/com/a101/ecofarming/global/notification/fcm/FirebaseConfig.java
+++ backend/src/main/java/com/a101/ecofarming/global/notification/fcm/FirebaseConfig.java
@@ -4,8 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
