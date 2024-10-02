d35d7748e3c6affe65efc8abcdfbbc14cdc44689
김수빈 <u1qns@naver.com>
2024-10-02 14:05:10 +0900

    📦Chore: Axios가 자동 처리하는 부분 코드 정리

--- frontend/src/services/fcmService.js
+++ frontend/src/services/fcmService.js
@@ -5,11 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 export const submitToken = async (request) => {
     console.log(request);
     try {
-        const response = await axios.post(`${API_URL}/fcm/token`, request, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        const response = await axios.post(`${API_URL}/fcm/token`, request);
         return response.data;   
     } catch (error) {
         throw new Error('FCM 토큰 저장에 실패했습니다.');
@@ -19,11 +15,7 @@ export const submitToken = async (request) => {
 export const subscribe = async (request) => {
     console.log(request);
     try {
-        const response = await axios.post(`${API_URL}/fcm/subscribe`, request, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        const response = await axios.post(`${API_URL}/fcm/subscribe`, request);
         return response.data;   
     } catch (error) {
         throw new Error('FCM 구독에 실패했습니다.');
@@ -33,11 +25,7 @@ export const subscribe = async (request) => {
 export const unsubscribe = async (request) => {
     console.log(request);
     try {
-        const response = await axios.post(`${API_URL}/fcm/unsubscribe`, request, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        const response = await axios.post(`${API_URL}/fcm/unsubscribe`, request);
         return response.data;   
     } catch (error) {
         throw new Error('FCM 구독 취소에 실패했습니다.');
