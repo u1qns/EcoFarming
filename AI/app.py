1e4a0ac8117a53ba1c758ba88d2010f47b0f7373
김태경 <98coding@gmail.com>
2024-10-04 15:42:34 +0900

    fix : AI image path

--- AI/app.py
+++ AI/app.py
@@ -9,11 +9,9 @@ app = Flask(__name__)
 CORS(app)  # CORS 허용 설정
 
 # AI 예측을 수행하는 엔드포인트
-@app.route('/run-predict', methods=['POST'])
 def run_predict():
     try:
         data = request.json
-        print(data)
         image_url = data['image_url']
 
         # URL에서 이미지를 다운로드
@@ -23,15 +21,15 @@ def run_predict():
         # predict.py에서 예측 함수 호출
         model = predict.load_resnet50_model()
         image_tensor = predict.preprocess_image(image)
-        prediction = predict.predict_image(model, image_tensor)
+        prediction_idx = predict.predict_image(model, image_tensor)
+
+        # 예측된 클래스 인덱스를 클래스 이름으로 변환
+        predicted_label = predict.get_class_label(prediction_idx)
 
         # AI 예측 결과 반환
-        result = {"aiPass": prediction}
+        result = {"aiPass": predicted_label}  # 여기서 label을 반환
         return jsonify(result)
     except Exception as e:
         # 예외 발생 시 오류 메시지를 Flask 콘솔에 출력
         print(f"Error during prediction: {e}")
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
