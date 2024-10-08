4d0c0788e476806546c2eef39f9b2684ad49a942
이효림 <hlleee@naver.com>
2024-10-08 13:51:11 +0900

    🐛Fix: 결제하기 페이지 뒤로가기 버튼

--- frontend/src/components/PaymentNavbar.js
+++ frontend/src/components/PaymentNavbar.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./Navbar.css";
-import { ChevronRight, Bell, Settings } from "lucide-react";
+import { ChevronRight, Bell, Settings, ArrowLeft  } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const PaymentNavbar = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1); // 이전 페이지로 이동
+
+  };
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <div className="navbar-title">
+      <div >
+          <ArrowLeft size={24} onClick={handleBackClick} className="back-arrow" />
+        </div>
+        <div className="navbar-title" style={{marginRight:150}}>
           <p>결제하기</p>
         </div>
-        <ul className="navbar-menu">
-          <li>
-            <Bell size={24} />
-          </li>
-          <li>
-            <Settings size={24} />
-          </li>
-        </ul>
+       
       </div>
     </nav>
   );
