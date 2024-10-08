6e94ba73752354499c66ba42ebbd640d23ad6eae
김수빈 <u1qns@naver.com>
2024-10-08 13:59:09 +0900

    🐛Fix: url 누락 추가

--- frontend/src/services/AIService.js
+++ frontend/src/services/AIService.js
@@ -4,7 +4,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export const submitAIResult = async (request) => {
     try {
-        const response = await axios.post(`${API_URL}/proof/AI`, request);
+        const response = await axios.post(`${API_URL}/complaints/proof/AI`, request);
         return response.data;
     } catch (error) {
         throw new Error('AI 결과 전송에 실패했습니다.' + error.message);
