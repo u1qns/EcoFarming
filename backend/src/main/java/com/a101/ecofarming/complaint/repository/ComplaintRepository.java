a588fb6273d84f8af0caf77d8743ef76eb2ce711
김수빈 <u1qns@naver.com>
2024-10-09 16:04:22 +0900

    ✨Feat: 내 신고 시간을 내림차순으로 정렬 & proofId 수정

--- backend/src/main/java/com/a101/ecofarming/complaint/repository/ComplaintRepository.java
+++ backend/src/main/java/com/a101/ecofarming/complaint/repository/ComplaintRepository.java
@@ -1,9 +1,10 @@
 package com.a101.ecofarming.complaint.repository;
 
 import com.a101.ecofarming.complaint.entity.Complaint;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
-    List<Complaint> findByUserId(Integer userId);
+    List<Complaint> findByUserId(Integer userId, Sort sort);
 }
